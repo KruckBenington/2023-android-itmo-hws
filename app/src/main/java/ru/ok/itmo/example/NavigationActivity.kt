6d7c064acd48c57f0dcd641732e09a2644a40794
Pletnev_Kirill <334139@niuitmo.ru>
2023-10-30 16:09:32 +0300

    Task 3: not all done

--- app/src/main/java/ru/ok/itmo/example/NavigationActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/NavigationActivity.kt
@@ -1,27 +1,61 @@
 package ru.ok.itmo.example
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class NavigationActivity : AppCompatActivity(R.layout.activity_navigation) {
 
+    private lateinit var navigationView: NavigationBarView
+    private lateinit var textView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tv = findViewById<TextView>(R.id.tv_fragment)
-        val navView = findViewById<BottomNavigationView>(R.id.navigation_panel)
-        val fragmentManager = supportFragmentManager
-        fragmentManager.commit {
-            setReorderingAllowed(true)
-            add(SwitchFragment(), "fragment_a")
-            addToBackStack("fragment_a")
-            add(SwitchFragment(), "fragment_b")
-            addToBackStack("fragment_b")
+        if (savedInstanceState == null) {
+            commitFragment(R.string.fragment_a)
+        }
+        navigationView = findViewById(R.id.navigation_panel)
+        navigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_a -> {
+                    commitFragment(R.string.fragment_a)
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.action_b -> {
+                    commitFragment(R.string.fragment_b)
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.action_c -> {
+                    commitFragment(R.string.fragment_c)
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> false
+            }
         }
 
 
+    }
 
+    private fun commitFragment(label: Int) {
+        val fragmentName = getString(label)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(
+                R.id.fragment_to_update, SwitchFragment::class.java, bundleOf(
+                    "label" to fragmentName
+                )
+            )
+            addToBackStack(fragmentName)
+        }
     }
 }
--- app/src/main/java/ru/ok/itmo/example/SwitchFragment.kt
+++ app/src/main/java/ru/ok/itmo/example/SwitchFragment.kt
@@ -1,11 +1,11 @@
 package ru.ok.itmo.example
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class SwitchFragment : Fragment() {
     private lateinit var viewModel: SwitchViewModel
@@ -19,8 +19,8 @@ class SwitchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        val textView = getView()?.findViewById<TextView>(R.id.tv_fragment)
+        
     }
 
 }
